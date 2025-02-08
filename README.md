18b1c2ee408f88f51bfdf379b2485273f2d2459b
Tega96 <otiteotega@gmail.com>
2025-02-08 07:48:03 +0100

    include additional endpoint to the get method, fix bugs, include README.md, add more description to the package.json

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-This is a readme file of my api file
+This is a public API that returns the following information:
+    1. My email address
+    2. Current datetime
+    3. My Github repo
+
+
+# Running the app
+    - Clone the github repository
+    - Cd into the file
+    - Instal node: npm install
+    - Start the server: node app.js
+    - Access the api on localhost:3000
+
+    0r 
+
+    - Click this link to access the api
+
+    or 
+
+    - You can test the API using a browser, curl, or any HTTP client like Postman:
+    curl http://localhost:3000/user
--- app.js
+++ app.js
@@ -3,18 +3,22 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.use(express.JSON());
+app.use(express.json());
 
 app.get('/', (req, res) => {
+	res.send("Welcome to my API, My name is Otega")
+})
+
+app.get('/user', (req, res) => {
 	const currentTime = new Date().toISOString();
 	const result = {
-		email = "otiteotega@gmail.com",
+		email : "otiteotega@gmail.com",
 		current_time : currentTime,
 		github_url  : "https://github.com/Tega96/HNG_API" 
 	}
-	res.JSON(result);
+	res.json(result);
 });
 
-app.listen(port, (req, res) => {
-	console.log(`Server is listening on port ${port};
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}`);
 });
